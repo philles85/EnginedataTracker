79c9501c200798a1244473ef58be4aa1434fb0d0
philles85 <philster04@outlook.com>
2026-03-29 01:12:23 +0100

    Added dbRead and dbService
    dbRW only function is to connect, read and write to database. dbService is a handshake between main request service to access db, and handles all services between read and write of request data.

--- API/middleware/middleware.php
+++ API/middleware/middleware.php
@@ -4,11 +4,11 @@ class Middleware {
 
     static function cors(){
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: *");
+        header("Access-Control-Allow-Methods: *");
         header("Access-Control-Allow-Headers: *");
 
     }
-
+    
 }
 
 
