5ad0bb006ee8c776f2d87f2f953d1baa350e9e4d
philles85 <philster04@outlook.com>
2026-04-03 23:21:55 +0200

    Some comments for service

--- API/services/engineValuesService.php
+++ API/services/engineValuesService.php
@@ -8,6 +8,9 @@ class EngineValuesService {
     }
 
     static function getByTime($input){
+        // Denna get requesten hämtar utifrån var det är för tid, och tanken är då att den ska ta nuvarande tid klockslag för att hämta
+        // det typ live per minut och klockslag typ. Och datan i databasen har pushats in med nuvarande, alltså de nya värderna
+        // och då hämtar den direkt efter nuvarande tid för att få det senaste hela tiden typ live, tror det funkar, finns kanske bättre sätt för detta
         $DBRep = new dbRW();
         $DB = $DBRep->connectDB();
         $DBObj = $DBRep->readTable("engine_values", "time", $input);
