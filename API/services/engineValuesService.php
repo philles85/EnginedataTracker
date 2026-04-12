ce0b22a9a87e10004da39cb206d3bd43636a4fb6
philles85 <philster04@outlook.com>
2026-04-12 15:07:07 +0200

    Updated EngineValuesService, one method for both instances

--- API/services/engineValuesService.php
+++ API/services/engineValuesService.php
@@ -7,32 +7,34 @@ class EngineValuesService {
 
     }
 
-    static function getByTime($input){
-        // Denna get requesten hämtar utifrån var det är för tid, och tanken är då att den ska ta nuvarande tid klockslag för att hämta
-        // det typ live per minut och klockslag typ. Och datan i databasen har pushats in med nuvarande, alltså de nya värderna
-        // och då hämtar den direkt efter nuvarande tid för att få det senaste hela tiden typ live, tror det funkar, finns kanske bättre sätt för detta
-        $DBRep = new dbRW();
-        $DB = $DBRep->connectDB();
-        $DBObj = $DBRep->readTable("engine_values", "time", $input);
+    // // OBS!! Denna används inte Ta bort sen
+    // static function getByTime($input){
+    //     // Denna get requesten hämtar utifrån var det är för tid, och tanken är då att den ska ta nuvarande tid klockslag för att hämta
+    //     // det typ live per minut och klockslag typ. Och datan i databasen har pushats in med nuvarande, alltså de nya värderna
+    //     // och då hämtar den direkt efter nuvarande tid för att få det senaste hela tiden typ live, tror det funkar, finns kanske bättre sätt för detta
+    //     $DBRep = new dbRW();
+    //     $DB = $DBRep->connectDB();
+    //     $DBObj = $DBRep->readTable("engine_values", "time", $input);
 
-        // Kolla om det ska vara empty eller något annat här
-        if(empty($DBObj)){
-            throw Exception(["error" => "No times matching"]);
+    //     // Kolla om det ska vara empty eller något annat här
+    //     if(empty($DBObj)){
+    //         throw Exception(["error" => "No times matching"]);
+    //     } else {
+    //         return $DBObj;
+    //     }
+
+    // }
+
+    static function getEngineValue($input){
+
+        $dbService = new dbService();
+
+        if(empty($input)){
+            $serviceGetAll = $dbService->getAllTable("engine_values");
+            return $serviceGetAll;
         } else {
-            return $DBObj;
-        }
-
-    }
-
-    static function getAll(){
-        $DBRep = new dbRW();
-        $DBConnect = $DBRep->connectDB();
-        $DBTable = $DBRep->readTable("engine_values");
-
-        if(empty($DBTable)){
-            throw Exception(["error" => "table is empty"]);
-        } else {
-            return $DBTable;
+            $serviceGetByTime = $dbService->getByColumn("engine_values", "time", $input["time"]);
+            return $serviceGetByTime;
         }
 
     }
